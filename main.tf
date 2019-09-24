b0da8e92bc55b3d43741d8079f464a408f9c2377
Jeremy Genicot (Europe) <jeremy.genicot@global.ntt>
2019-09-24 17:09:20 +0200

    add equal sign to tags

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "my-test-instance" {
   ami             = "${data.aws_ami.amazon-linux-2.id}"
   instance_type   = "t2.micro"
 
-  tags {
+  tags = {
     Name = "test-instance"
   }
 }
